81c3a254275f392146ce2f1f6751497b5fff606d
priya shukla <priyashukla22@navgurukul.org>
2024-02-09 15:03:22 +0530

    first commit

--- weather.js
+++ weather.js
@@ -19,7 +19,7 @@ async function cheackWeather(city){
 
   var data=await response.json();
 
-  // console.log(data);
+ 
   document.querySelector(".city").innerHTML=data.name;
   document.querySelector(".temp").innerHTML= Math.round(data.main.temp)+"°C";
   document.querySelector(".humidity").innerHTML=data.main.humidity+"%";
@@ -48,61 +48,6 @@ searchBtn.addEventListener("click",()=>{
 });
 
 
-// const apiKey = "5327e387f438b4f1f6d673e5839d9df3"
-// const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
-// const searchbox = document.querySelector(".search input");
-// const searchbtn = document.querySelector(".search button");
-// const weatherIcon = document.querySelector(".weather-icon");
-// const dateElement = document.querySelector(".date");
-// const clockElement = document.getElementById('clock');
-// function displayTodayDate() {
-//   const options = {weekday: "long", month: "long", day: "numeric", year: "numeric" };
-//   const today = new Date();
-//   dateElement.innerHTML = today.toLocaleDateString(undefined, options);
-//   const timing = {
-//       hour: '2-digit',
-//       minute: '2-digit',
-//       second: '2-digit',
-//   };
-//   const timeString = today.toLocaleTimeString('en-US', timing);
-//   clockElement.textContent = timeString;
-//   console.log(today);
-// }
-// async function checkWeather(city) {
-//   const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-//   if (response.status == 404 ) {
-//     document.querySelector(".error").style.display = "block";
-//     document.querySelector(".weather").style.display = "none";
-//   }
-//   else {
-//     document.querySelector(".weather").style.display = "block";
-//   }
-//   var data = await response.json();
-//   console.log(data);
-//   document.querySelector(".city").innerHTML = data.name;
-//   document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
-//   document.querySelector(".weathertype").innerHTML = data.weather[0].main;
-//   document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-//   document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
-//   if (data.weather[0].main == "Clouds") {
-//     weatherIcon.src = "img/clouds.png";
-//   } else if (data.weather[0].main == "Clear") {
-//     weatherIcon.src = "img/clear.png";
-//   } else if (data.weather[0].main == "Rain") {
-//     weatherIcon.src = "img/rain.png";
-//   } else if (data.weather[0].main == "Drizzle") {
-//     weatherIcon.src = "img/drizzle.png";
-//   } else if (data.weather[0].main == "Mist") {
-//     weatherIcon.src = "img/mist.png";
-//   }
-//   document.querySelector(".error").style.display = "none";
-//   document.querySelector(".weather").style.display = "block";
-// }
-// searchbtn.addEventListener("click", () => {
-//   checkWeather(searchbox.value);
-//   displayTodayDate();
-//   setInterval(displayTodayDate, 1000);
-// });
 
 
 
